401013be41383879a135f26edc02156c142f3705
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-05-01 22:50:12 -0300

    feat: Add v-table

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,50 @@
 <template>
   <div id="app">
-    <ul>
-      <li v-for="person in dataUsers" :key="person.name">
-        {{ person.name }}
-        {{ person.phone }}
-      </li>
-    </ul>
+    <h1>Users list</h1>
+
+    <VTable
+      class="v-table"
+      :theadCols="[
+        { text: 'Name', width: 200 },
+        { text: 'Phone', width: 200 },
+        { text: 'Action', width: 100 },
+      ]"
+    >
+      <template #tbody>
+        <tr v-for="person in dataUsers" :key="person.name">
+          <td>{{ person.name }}</td>
+          <td>{{ person.phone }}</td>
+          <td>
+            <button>Edit</button>
+            <button>Delete</button>
+          </td>
+        </tr>
+      </template>
+    </VTable>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import dataUsers from "@/api/index";
+import VTable from "@/components/ui/VTable.vue";
 
 export default Vue.extend({
   name: "App",
+  components: { VTable },
   data: () => ({
     dataUsers,
   }),
 });
 </script>
+
+<style lang="scss" scoped>
+#app {
+  text-align: center;
+
+  .v-table {
+    margin: 0 auto;
+    max-width: 800px;
+  }
+}
+</style>
---
+++ src/components/ui/VTable.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="table-wrapper">
+    <table class="table">
+      <thead>
+        <tr>
+          <th
+            v-for="col in theadCols"
+            :key="col.text"
+            :style="{ width: col.width }"
+          >
+            {{ col.text }}
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <slot name="tbody"></slot>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+interface ITheadCols {
+  text: string;
+  width: string | number;
+}
+
+export default Vue.extend({
+  name: "VTable",
+  props: {
+    theadCols: { type: Array as () => ITheadCols[], required: true },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/_variables";
+
+.table-wrapper {
+  $clr-table-border: #9b9b9b;
+  display: block;
+  overflow-x: auto;
+  margin-bottom: 1rem;
+  box-shadow: 0 0 2px 0 $clr-table-border;
+
+  .table {
+    table-layout: fixed;
+    border-collapse: collapse;
+    width: 100%;
+    font-size: 0.95rem;
+    word-wrap: break-word;
+    text-align: left;
+    background-color: #fff;
+  }
+
+  .table thead {
+    $clr-bg-thead: #e5e7eb;
+    background-color: $clr-bg-thead;
+    border-bottom: solid 2px $clr-table-border;
+
+    th {
+      position: sticky;
+      top: -1px;
+      padding: 0.75rem 0.5rem;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+      font-weight: bold;
+      background-color: $clr-bg-thead;
+      vertical-align: middle;
+    }
+  }
+
+  .table tbody {
+    tr:nth-child(even) {
+      background-color: $grey;
+    }
+
+    td {
+      padding: 0.6rem 0.5rem;
+      vertical-align: middle;
+    }
+  }
+}
+</style>
